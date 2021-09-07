e314878b1b6d57b08554315e37579e7c5f372298
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2021-09-07 12:43:23 +0200

    Added rnvectoricons deps; Created Icon component;

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Icon = (): JSX.Element => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default Icon;
