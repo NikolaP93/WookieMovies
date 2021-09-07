bcb983e0e494f0cd2724134241f57bbed53734fd
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2021-09-07 13:07:04 +0200

    Added search query API, added return value types for get requests;

--- src/modules/services/API.ts
+++ src/modules/services/API.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
 const URL = 'https://wookie.codesubmit.io/movies';
+const SEARCH_QUERY_URL = '?q=';
 
 const config = {
   headers: {
@@ -9,8 +10,12 @@ const config = {
 };
 
 class APIService {
-  async getMovies() {
+  async getMovies(): Promise<AxiosResponse> {
     return axios.get(URL, config);
+  }
+
+  async queryMovies(query: string): Promise<AxiosResponse> {
+    return axios.get(URL + SEARCH_QUERY_URL + query, config);
   }
 }
 
