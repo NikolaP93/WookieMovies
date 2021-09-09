423444c6bc07a1664a5ef7dcbb4f2d7e49b09ba3
Nikola Prijovic <nikolaprijovic@Nikolas-MacBook-Air.local>
2021-09-09 16:55:56 +0200

    Added Flash Message component;

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './src/navigation/Tab/TabNavigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import ContextProvider from './src/modules/context/AppContext';
+import FlashMessage from 'react-native-flash-message';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <ContextProvider>
+          <FlashMessage position="top" />
           <TabNavigation />
         </ContextProvider>
       </NavigationContainer>
